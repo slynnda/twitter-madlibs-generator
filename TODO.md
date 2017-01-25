cec4916406cf766edafb5bb313f59c78ae5e64c9
Jason Hills <jasonahills@gmail.com>
2017-01-24 19:51:24 -0700

    adds MadLib types

---
+++ TODO.md
@@ -0,0 +1,11 @@
+# Front-end
+[ ] get madlib from server
+[ ] ui for filling in blanks
+[ ] display result
+
+# Back-end
+[ ] get tweet
+[ ] nlp tweet
+[ ] transform to madlib
+
+# Common
---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type PartOfSpeech = 'Noun' | 'Verb' | 'Adjective' | 'Adverb'
+
+export type UnfilledMadLib = (MadLibTextBlock | MadLibUnfilledSpace)[]
+export type FilledMadLib = (MadLibTextBlock | MadLibFilledSpace)[]
+
+export type MadLibTextBlock = {
+    text: string
+}
+
+export type MadLibUnfilledSpace = {
+    partOfSpeech: PartOfSpeech
+}
+
+export type MadLibFilledSpace = {
+    partOfSpeech: PartOfSpeech
+    text: string
+}
